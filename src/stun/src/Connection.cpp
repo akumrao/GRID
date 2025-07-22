9a9f9b2b5d3a5e6810616d5c659acb2da4082eab
Arvind Umrao <akumrao@yahoo.com>
2025-07-23 00:07:31 +0530

    added mapping ipv6 to ipv4

--- src/stun/src/Connection.cpp
+++ src/stun/src/Connection.cpp
@@ -22,6 +22,29 @@ int testUdpServer::agent_direct_send( uint8_t* data, uint32_t nbytes, addr_recor
     return udpServer->send( (char*) data, nbytes , (const struct sockaddr*)&record.addr);
 }
       
+
+
+bool addr_unmap_inet6_v4mapped(struct sockaddr *sa, socklen_t *len) {
+	if (sa->sa_family != AF_INET6)
+		return false;
+
+	const struct sockaddr_in6 *sin6 = (const struct sockaddr_in6 *)sa;
+	if (!IN6_IS_ADDR_V4MAPPED(&sin6->sin6_addr))
+		return false;
+
+	struct sockaddr_in6 copy = *sin6;
+	sin6 = &copy;
+
+	struct sockaddr_in *sin = (struct sockaddr_in *)sa;
+	memset(sin, 0, sizeof(*sin));
+	sin->sin_family = AF_INET;
+	sin->sin_port = sin6->sin6_port;
+	memcpy(&sin->sin_addr, ((const uint8_t *)&sin6->sin6_addr) + 12, 4);
+	*len = sizeof(*sin);
+	return true;
+}
+
+
 void testUdpServer::OnUdpSocketPacketReceived(UdpServer* socket, const char* data, size_t len,  struct sockaddr* remoteAddr) {
 
     int family;
@@ -38,6 +61,7 @@ void testUdpServer::OnUdpSocketPacketReceived(UdpServer* socket, const char* dat
     
     addr_record_t src;
     
+    addr_unmap_inet6_v4mapped( remoteAddr , (socklen_t *)&len);
     IP::CopyAddress(remoteAddr, src );
     
     agent->onStunMessage((unsigned char *)data, len,  &src, nullptr );
