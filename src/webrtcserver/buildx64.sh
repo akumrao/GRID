eb0f18af417eff930715571b98614088fc37ec1d
Arvind Umrao <akumrao@yahoo.com>
2025-08-19 23:01:04 +0530

    added data channel to peerconnection

--- src/webrtcserver/buildx64.sh
+++ src/webrtcserver/buildx64.sh
@@ -5,7 +5,7 @@ mkdir -p build
 
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DWEBRTC_REPO=/workspace/MagicAI/src/webrtcserver -DCMAKE_POSITION_INDEPENDENT_CODE=ON  .. 
+cmake -DCMAKE_BUILD_TYPE=Debug -DWEBRTC_REPO=/workspace/MagicAI/src/webrtcserver  -DWORKSPACE=/workspace/MagicAI  -DCMAKE_POSITION_INDEPENDENT_CODE=ON  .. 
 
 
 make -j$(nproc)
