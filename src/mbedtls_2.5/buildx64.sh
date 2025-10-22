6b5035458971a5f5cd6e2939eb86141a4cb16163
Arvind Umrao <akumrao@yahoo.com>
2025-10-22 22:33:05 +0530

    upgreaded to mbedtls 3.64

---
+++ src/mbedtls_2.5/buildx64.sh
@@ -0,0 +1,14 @@
+
+rm -rf build
+
+mkdir -p build
+
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=Debug .. -DENABLE_TESTING=OFF -DENABLE_PROGRAMS=ON 
+
+#cmake -DENABLE_TESTING=Off -DCMAKE_BUILD_TYPE=MinSizeRel ..  -DENABLE_TESTING=OFF -DENABLE_PROGRAMS=OFF  -DUSE_SHARED_MBEDTLS_LIBRARY=1
+
+make -j32
+
+cd ..
