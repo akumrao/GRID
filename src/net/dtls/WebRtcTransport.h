cd03db575445c23f2b07ce49ac057f82336bdf2b
Arvind Umrao <akumrao@yahoo.com>
2025-12-07 22:59:40 +0530

    added class for dtls and webrtc

---
+++ src/net/dtls/WebRtcTransport.h
@@ -0,0 +1,94 @@
+#ifndef WEBRTC_TRANSPORT_HPP
+#define WEBRTC_TRANSPORT_HPP
+
+#include "DtlsTransport.h"
+
+//#include "SrtpSession.h"
+//#include "RTC/StunPacket.h"
+#include "net/netInterface.h"
+#include "net/TcpConnection.h"
+#include "net/TcpServer.h"
+#include "TransportTuple.h"
+#include "net/UdpSocket.h"
+#include <vector>
+
+
+namespace RTC
+{
+	class WebRtcTransport :  public  base::net::UdpServer::Listener,
+	                       // public RTC::TcpServer::Listener,
+	                       // public RTC::TcpConnection::Listener,
+                                public base::net::Listener,
+	                        public RTC::DtlsTransport::Listener
+	{
+	private:
+		struct ListenIp
+		{
+			std::string ip;
+			std::string announcedIp;
+		};
+
+	public:
+		WebRtcTransport(const std::string& id );
+		~WebRtcTransport() ;
+
+	public:
+
+
+	private:
+		bool IsConnected() const ;
+		void MayRunDtlsTransport();
+                void HandleRequest();
+
+		//void SendSctpData(const uint8_t* data, size_t len) override;
+		void OnPacketReceived(base::net::TransportTuple* tuple, const char* data, size_t len);
+		void OnStunDataReceived(base::net::TransportTuple* tuple, const char* data, size_t len);
+		void OnDtlsDataReceived(const base::net::TransportTuple* tuple, const char* data, size_t len);
+
+
+		/* Pure virtual methods inherited from RTC::UdpSocket::Listener. */
+	public:
+		void OnUdpSocketPacketReceived(
+		   base::net::UdpServer* socket, const  char* data, size_t len,  struct sockaddr* remoteAddr) override;
+
+		/* Pure virtual methods inherited from RTC::TcpServer::Listener. */
+	public:
+		//void OnRtcTcpConnectionClosed(RTC::TcpServer* tcpServer, RTC::TcpConnection* connection) override;
+		void on_close( base::net::Listener* conn);
+		/* Pure virtual methods inherited from RTC::TcpConnection::Listener. */
+	public:
+//		void OnTcpConnectionPacketReceived(
+//		  RTC::TcpConnection* connection, const uint8_t* data, size_t len) override;  //
+                 void on_read( base::net::Listener* conn, const char* data, size_t len);
+		/* Pure virtual methods inherited from RTC::IceServer::Listener. */
+	public:
+
+
+		/* Pure virtual methods inherited from RTC::DtlsTransport::Listener. */
+	public:
+		void OnDtlsTransportConnecting(const RTC::DtlsTransport* dtlsTransport) override;
+                void OnDtlsTransportConnected( const RTC::DtlsTransport* /*dtlsTransport*/) override;
+		
+		void OnDtlsTransportFailed(const RTC::DtlsTransport* dtlsTransport) override;
+		void OnDtlsTransportClosed(const RTC::DtlsTransport* dtlsTransport) override;
+		void OnDtlsTransportSendData(
+		  const RTC::DtlsTransport* dtlsTransport, const uint8_t* data, size_t len) override;
+		void OnDtlsTransportApplicationDataReceived(
+		  const RTC::DtlsTransport* dtlsTransport, const uint8_t* data, size_t len) override;
+
+	private:
+		// Allocated by this.
+
+		// Map of UdpSocket/TcpServer and local announced IP (if any).
+		std::unordered_map<base::net::UdpServer*, std::string> udpSockets;
+		std::unordered_map<base::net::TcpServer*, std::string> tcpServers;
+		RTC::DtlsTransport* dtlsTransport{ nullptr };
+		
+		// Others.
+		bool connectCalled{ false }; // Whether connect() was succesfully called.
+
+		RTC::DtlsTransport::Role dtlsRole{ RTC::DtlsTransport::Role::AUTO };
+	};
+} // namespace RTC
+
+#endif
---
+++ src/net/dtlstest/testDtlsClient.cpp
@@ -0,0 +1,87 @@
+/* This file is part of mediaserver. A webrtc sfu server.
+ * Copyright (C) 2018 Arvind Umrao <akumrao@yahoo.com> & Herman Umrao<hermanumrao@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+#include "base/base.h"
+#include "base/logger.h"
+#include "base/application.h"
+#include "net/netInterface.h"
+#include "net/SslConnection.h"
+// #include "base/test.h"
+#include "base/time.h"
+
+//#include "net/sslmanager.h"
+
+using std::endl;
+using namespace base;
+using namespace net;
+// using namespace base::test;
+
+
+class tesTcpClient : public SslConnection {
+public:
+
+    tesTcpClient(): SslConnection(false)
+    {
+        Connect("127.0.0.1", 5001);
+    }
+
+
+
+    void on_close() {
+
+        std::cout << " Close Con LocalIP" << this->GetLocalIp() << " PeerIP" << this->GetPeerIp() << std::endl << std::flush;
+
+    }
+
+    void on_read( const char* data, size_t len) {
+        
+      std::cout << " on_read " << this->GetLocalIp() << " PeerIP " << this->GetPeerIp() << std::endl << std::flush;
+
+        std::cout << "data: " << data << " len: " << len << std::endl << std::flush;
+        std::string send = "12345";
+       // connection->send((const char*) send.c_str(), 5);
+
+    }
+    
+    void on_connect() {
+        
+       SslConnection::on_connect();
+        
+       std::cout << " on_read " << this->GetLocalIp() << " PeerIP " << this->GetPeerIp() << std::endl << std::flush;
+
+     
+       std::string send = "12345";
+       SslConnection::send((const char*) send.c_str(), 5);
+       std::cout << "TCP Client send data: " << send << "len: " << strlen((const char*) send.c_str()) << std::endl << std::flush;
+
+    }
+    
+  
+
+};
+
+int main(int argc, char** argv) {
+    Logger::instance().add(new ConsoleChannel("debug", Level::Trace));
+
+      // net::SSLManager::initNoVerifyClient();
+       
+        Application app;
+
+        tesTcpClient socket;
+
+        app.run();
+
+        
+
+
+
+
+    return 0;
+}
