6251e397cec917fa8f1c37e6c48e3f879acc23f4
Arvind Umrao <akumrao@yahoo.com>
2025-12-10 22:42:21 +0530

    removed hard coded of ssl

--- src/broadcast/main/config.js
+++ src/broadcast/main/config.js
@@ -4,7 +4,7 @@
     "log": "/var/log/",
     "qrcode":"65f570720af337cec5335a70ee88cbfb7df32b5ee33ed0b4a896a0",
     "server": "localhost",
-    "port": 443,
+    "port": 9000,
     "recording": true,
     "cloud": false,
     "facedetect": true,
