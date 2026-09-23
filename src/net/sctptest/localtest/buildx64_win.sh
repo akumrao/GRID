d5336128bdce43f310995d29481daed7110a84ee
Arvind Umrao <akumrao@yahoo.com>
2026-09-24 00:29:01 +0530

    fixed build scripts

--- src/net/sctptest/localtest/buildx64_win.sh
+++ src/net/sctptest/localtest/buildx64_win.sh
@@ -1,11 +1,11 @@
 
 rm -rf build
-mkdir -p build
+mkdir -p  build/Debug
 
 cp cache.js git add certificate.crt  private_key.pem index.html build/
-
+cp cache.js git add certificate.crt  private_key.pem index.html build/Debug
 
 cmake -B build -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Debug -DUSE_MBEDTLS=1 -DRTC_STATIC=1 -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
-
+cmake --build build --config Debug
 
 cd ..
